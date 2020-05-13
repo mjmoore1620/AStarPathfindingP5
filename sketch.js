431e5cc2b4cc27d0a3157a0523fcb3962087b07e
Matt Moore <zmjm1320@gmail.com>
2020-05-13 13:56:32 -0400

    add grid with rectangles for nodes

--- sketch.js
+++ sketch.js
@@ -1,8 +1,30 @@
+let cols = 5;
+let rows = 5;
+let space = 20;
+let nodeSize = 5;
+
+let grid = new Array(cols);
+
 function setup() {
 	createCanvas(800, 600);
 	background(51);
+
+
+	cols = width / space;
+	rows = height / space;
+
+	for (let i = 0; i < cols; i++) {
+		grid[i] = new Array(rows);
+	}
+
 }
 
 function draw() {
+	background(51);
+	for (let i = 0; i < grid.length; i++) {
+		for (let j = 0; j < grid[i].length; j++) {
+			rect(i * space, j * space, nodeSize, nodeSize);
+		}
+	}
 
 }
