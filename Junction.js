c6cddb1f9f63f909a3b258ed5fb8012063546280
Matt Moore <zmjm1320@gmail.com>
2020-05-23 00:53:49 -0400

    add variables f, g, h from cost equation f(n) = g(n) + h(n), add color to show function

--- Junction.js
+++ Junction.js
@@ -1,3 +1,6 @@
+/**
+ * Class that represents a node in a A* pathfinding demo. Would have named it node, but it was trying to use node.js the server framework
+ */
 class Junction {
     constructor(x, y, size, scale) {
         this.position = createVector(x, y);
@@ -5,10 +8,50 @@ class Junction {
         this.size = size;
         this.scale = scale;
         this.margin = size * 3;
+        this.start;
+        this.goal;
+
+        //f(n) = g(n) + h(n)
+        //estimate from this node to goal
+        this.h;
+        //cost of path from parent to start
+        this.g;
+        //g + h
+        this.f;
+    }
+
+    setH(goal) {
+        this.h = dist(this.position, goal.position);
+    }
+    
+    setG(parent) {
+        this.g = dist(this.position, parent.position) + parent.gCost;
+    }
+
+    setF(num) {
+        //this.f = this.g + this.h;
+        this.f = num;
     }
 
     show() {
+        if (this.start) {
+            stroke('green');            
+            fill('green');
+        }
+        else if (this.goal) {
+            stroke('red')
+            fill('red');
+        }
+        else {
+            stroke('black')
+            fill('black');
+        }
+        
         rect(this.position.x * this.scale + this.margin, this.position.y * this.scale + this.margin, this.size, this.size);
+    }
+
+    valueOf() {
+        return this.f;
     }
 
 }
